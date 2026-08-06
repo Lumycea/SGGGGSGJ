2b273b1ef5e3cdeae7ef42308ebea0712f6b254b
HackFight <alyx.cornu@gmail.com>
2026-08-06 20:13:19 +0200

    Banner

--- Assets/Scripts/Banner.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Banner : Item
-{
-    public override int MaxStackCount => 1;
-
-    public override Sprite Sprite => Resources.Load<Sprite>("Items/MilkShake");
-}
